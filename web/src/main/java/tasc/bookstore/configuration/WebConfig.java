c91b99d165042b49ece2d421fa2ad14fd6adbd5d
Vu Linh Dev <linh.nv0201@gmail.com>
2025-09-25 17:30:26 +0700

    add web-config

---
+++ web/src/main/java/tasc/bookstore/configuration/WebConfig.java
@@ -0,0 +1,23 @@
+package tasc.bookstore.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Áp dụng cho tất cả các endpoint
+                .allowedOrigins(
+                        "http://localhost:4200",
+                        "127.0.0.1:5500",
+                        "https://clothing-dms-aay8aa4l1-vu-linhs-projects-83bba419.vercel.app",
+                ) // Chỉ cho phép yêu cầu từ domain này
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*") // Cho phép tất cả các header
+                .allowCredentials(true); // Cho phép cookies hoặc thông tin xác thực
+    }
+}
